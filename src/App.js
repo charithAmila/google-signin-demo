7707eb2d09865270b351edbf25e31cc0838b8a5f
charithAmila <dkcadissanayaka@gmail.com>
2021-05-05 23:13:37 +0530

    Google integration

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useGoogleLogin } from "react-google-login";
+import "./App.css";
 
 function App() {
+  const onSuccess = (data) => {
+    console.log("data", data);
+  };
+  const onFailure = (data) => {
+    console.log("OnFailue", data);
+  };
+  const { signIn, loaded } = useGoogleLogin({
+    onSuccess,
+    clientId:
+      "871153974453-gujtm07ngn2hmff4dpjjim4ukoepf2gk.apps.googleusercontent.com",
+    // scope,
+    onFailure,
+  });
+  console.log("loaded", loaded);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={signIn}>Google login</button>
       </header>
     </div>
   );
